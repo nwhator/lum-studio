50aaf6a7ae28fea1f4169b53951aa4acf23f1413
nwhator <nwhator@gmail.com>
2025-10-06 13:08:00 +0100

    Header

--- src/pages/about/about-us.tsx
+++ src/pages/about/about-us.tsx
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 // internal imports
 import Wrapper from "@/layouts/wrapper";
-import HeaderEleven from "@/layouts/headers/header-eleven";
+import HeaderOne from "@/layouts/headers/header-one";
 import FooterTwo from "@/layouts/footers/footer-two";
 import AboutUsHero from "@/components/about/about-us-hero";
 import AboutUsArea from "@/components/about/about-us-area";
@@ -36,7 +36,7 @@ const AboutUsMain = () => {
   return (
     <Wrapper>
       {/* header area start */}
-      <HeaderEleven transparent={true} />
+      <HeaderOne transparent={true} />
       {/* header area end */}
 
       <div id="smooth-wrapper">
