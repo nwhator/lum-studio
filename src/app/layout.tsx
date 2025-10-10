0f971998a151f19aedd1fd489c5d724387a2d199
nwhator <nwhator@gmail.com>
2025-10-10 11:04:31 +0100

    Tools vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import {
   Syne,
   Aladin,
@@ -87,6 +88,7 @@ export default function RootLayout({
       >
       {children}
       <Analytics />
+      <SpeedInsights />
       </body>
     </html>
   );
